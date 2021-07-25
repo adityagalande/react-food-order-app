baf9ff5bec10cb0a59fcda2e0eef8ec915c9a575
Aditya Galande <adityagalande14@gmail.com>
2021-07-25 17:02:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>USA AND CANADA I AM DEFINITLY COMMING TO YOU SOON.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
