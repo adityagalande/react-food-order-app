d15533708b47b2f2b47bcc4acafc7bb19dc1588a
Aditya Galande <adityagalande14@gmail.com>
2021-07-26 23:28:34 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Cart from "./Component/Cart/Cart";
 import Header from "./Component/Layout/Header";
-
+import Meals from "./Component/Meals/Meals";
 function App() {
+  const [cartIsShow, setCartIsShow] = useState(false);
+
+  const showcartHandler = () => {
+    setCartIsShow(true);
+  };
+
+  const hidecartHandler = () => {
+    setCartIsShow(false);
+  };
+
   return (
     <React.Fragment>
-      <Header />
+      {cartIsShow && <Cart onClose={hidecartHandler} />}
+      <Header onShowCart={showcartHandler} />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
