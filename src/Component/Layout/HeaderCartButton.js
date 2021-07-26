4acab35c079fa0b83140449c087314690b127595
Aditya Galande <adityagalande14@gmail.com>
2021-07-26 23:28:11 +0530

    application component created

--- src/Component/Layout/HeaderCartButton.js
+++ src/Component/Layout/HeaderCartButton.js
@@ -1,10 +1,9 @@
-import "./HeaderCartButton.module.css";
+import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
-import { classes } from "istanbul-lib-coverage";
 
 const HeaderCartButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
